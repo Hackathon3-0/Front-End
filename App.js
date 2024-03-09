db69d3f27c6aeb48617d4b5ededff16c6f5f876c
ferhatergun <eferhat718@gmail.com>
2024-03-09 17:30:34 +0300

    iskelet

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// import { Checkbox } from 'native-base';
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-blue-500">
       <Text>Open up App.js to start working on your app!</Text>
+      {/* <Checkbox value="test" accessibilityLabel="This is a dummy checkbox" /> */}
       <StatusBar style="auto" />
     </View>
   );
